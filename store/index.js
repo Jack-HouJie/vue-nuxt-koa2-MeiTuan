b77ce63d42b48afcc67a364c5f0b5dccf95bd7cb
HouJie <houjie_jack@outlook.com>
2020-06-23 14:30:12 +0800

    修复显示BUG

--- store/index.js
+++ store/index.js
@@ -4,31 +4,23 @@ export const actions = {
   async nuxtServerInit ({
     commit
   }, { req, app }) {
-    // SSR渲染当前城市
     // 通过app拿到实例
     // 解构赋值异步Ajax请求，得到当前城市数据
     const { status, data: { province, city } } = await app.$axios.get('/geo/getPosition')
     // 调用在geo.js定义好的commit
     // 进行简单检查
     commit('geo/setPosition', status === 200 ? { city, province } : { city: '', province: '' })
-
-
-    // SSR渲染热门城市数据 bug
-    const { status: status3, data: { hotPlaceList } } = await app.$axios.get('/search/hotPlace', {
-      params: {
-        // 接口格式要求
-        city: app.store.state.geo.position.city.replace('市', '')
-      }
-    })
-    // 调用在home.js定义好的commmit
-    commit('home/setHotPlace', status3 === 200 ? hotPlaceList : [])
-
-    // SSR渲染菜单数据
+    // 解构赋值异步请求得到菜单数据
     const { status: status2, data: { menu } } = await app.$axios.get('/geo/menu')
     // 掉用在home.js定义好的commmit
     commit('home/setMenu', status2 === 200 ? menu : [])
-
-
-
+    // 解构赋值异步请求得到热门城市数据
+    const { status: status3, data: { result } } = await app.$axios.get('/search/hotPlace', {
+      params: {
+        city: app.store.state.geo.position.city.replace('市', '')
+      }
+    })
+    // 掉用在home.js定义好的commmit
+    commit('home/setHotPlace', status3 === 200 ? result : [])
   }
 }
